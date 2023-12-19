260220ec321a750271d5855cf167053c43a26c44
realJimboSlice <jimm7493@stud.kea.dk>
2023-12-19 23:03:25 +0100

    Did an npm run and played around with objects in script.js

--- 01_05/script.js
+++ 01_05/script.js
@@ -32,6 +32,40 @@ const backpack = {
     this.strapLength.right = lengthRight;
     updateBackpack(`Strap lengths updated.`);
   },
+  newColor: function (color) {
+    this.color = color;
+    updateBackpack(`Color updated.`);
+  },
+  newName: function (name) {
+    this.name = name;
+    updateBackpack(`Name updated.`);
+  },
+  newVolume: function (volume) {
+    this.volume = volume;
+    updateBackpack(`Volume updated.`);
+  },
+  newPocketNum: function (pocketNum) {
+    this.pocketNum = pocketNum;
+    updateBackpack(`Number of pockets updated.`);
+  },
+  newBackpack: function (
+    name,
+    volume,
+    color,
+    pocketNum,
+    lengthLeft,
+    lengthRight,
+    lidOpen
+  ) {
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+    this.lidOpen = lidOpen;
+    updateBackpack(`Backpack updated.`);
+  },
 };
 
 const markup = (backpack) => {
