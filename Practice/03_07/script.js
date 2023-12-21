6be5e3ac160ff461205a64aac7765c42eba91ba1
realJimboSlice <jimm7493@stud.kea.dk>
2023-12-22 00:21:08 +0100

    Added a few more objects like a Playstation 4 and a rain droplet :)

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -51,6 +51,74 @@ const phone = {
   },
 };
 
-console.log("cup object:", cup);
-console.log("guitar object:", guitar);
-console.log("phone:", phone);
+const candle = {
+  color: "white",
+  material: "wax",
+  isLit: true,
+  isMelted: false,
+  melt: function () {
+    this.isMelted = true;
+    console.log("Candle melted.");
+  },
+  light: function () {
+    this.isLit = true;
+    console.log("Candle lit.");
+  },
+  blowOut: function () {
+    this.isLit = false;
+    console.log("Candle blown out.");
+  },
+  isInContainer: true,
+  container: {
+    color: "translucent",
+    material: "glass",
+  },
+};
+
+const playstation4 = {
+  color: "black",
+  model: "PS4 Slim",
+  storage: "500GB",
+  hasDiscDrive: true,
+  hasDiscInside: false,
+  isOn: false,
+  powerButton: function () {
+    if (this.isOn == true) {
+      this.isOn = false;
+      console.log("PS4 turned off.");
+    } else {
+      this.isOn = true;
+      console.log("PS4 turned on.");
+    }
+  },
+  ejectDisc: function () {
+    if (this.hasDiscInside == true) {
+      this.hasDiscInside = false;
+      console.log("Disc ejected.");
+    } else {
+      console.log("No disc inside.");
+    }
+  },
+};
+
+const rainDroplet = {
+  size: "big",
+  color: "transparent",
+  isFalling: true,
+  isOnGround: false,
+  fall: function () {
+    this.isFalling = true;
+    console.log("Rain droplet falling.");
+  },
+  hitGround: function () {
+    this.isOnGround = true;
+    console.log("Rain droplet hit the ground.");
+  },
+};
+
+console.log("Cup:", cup);
+console.log("Guitar:", guitar);
+console.log("Phone:", phone);
+console.log("Candle:", candle);
+console.log("Playstation 4:", playstation4);
+console.log("Rai droplet:", rainDroplet);
